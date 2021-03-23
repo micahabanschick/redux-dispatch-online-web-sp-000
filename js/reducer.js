adda5995efde59a9c7310cbeba11ad35b2eda207
Micah Banschick <micha.ban@gmail.com>
2021-03-23 18:42:25 +0000

    Automatically backed up by Learn

--- js/reducer.js
+++ js/reducer.js
@@ -9,7 +9,21 @@ function changeState(state, action){
   }
 }
 
-let state = {count: 0}
+function dispatch(action) {
+  state = changeState(state, action)
+  render() 
+}
+
+function render() {
+  document.body.textContent = state.count 
+}
+
+render()
+
 let action = {type: 'INCREASE_COUNT'}
 
-changeState(state, action)
+dispatch({type: 'INCREASE_COUNT'})
+
+dispatch({type: 'INCREASE_COUNT'})
+  
+dispatch({type: 'INCREASE_COUNT'})
